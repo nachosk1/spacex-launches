0d73112ede6d672807c7e0812095eeeed3853e65
nachosk1 <104322849+nachosk1@users.noreply.github.com>
2024-03-06 09:02:21 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid',
-  adapter: vercelServerless(),
+  output: "hybrid",
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
 });
